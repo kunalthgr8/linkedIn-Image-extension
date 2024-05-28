1c81755253cd6134cc080ec4338a963faf98bebd
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-05-28 19:22:34 +0530

    Done

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const newImageUrl = request.imageUrl;
-  
-  // Select all profile pictures on the page
-  const profilePics = document.querySelectorAll('img');
 
-  profilePics.forEach(pic => {
+  // Select all profile pictures on the page
+  const profilePics = document.querySelectorAll("img");
+
+  profilePics.forEach((pic) => {
     // Replace the src attribute of each image with the new image URL
-    pic.src = newImageUrl;
+    if (pic.width < 70) {
+      pic.src = newImageUrl;
+    }
   });
 });
--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 // popup.js
 
-document.getElementById('replace-button').addEventListener('click', () => {
-    const imageUrl = document.getElementById('image-url').value;
-  
-    // Send a message to the background script
+document.getElementById("replace-button").addEventListener("click", () => {
+  const imageUrl = document.getElementById("image-url").value;
+  // Send a message to the background script
+  if (imageUrl) {
     chrome.runtime.sendMessage({ action: "replaceImages", imageUrl: imageUrl });
-  });
-  
+  }else{
+    alert("Please enter a valid image URL");
+  }
+});
